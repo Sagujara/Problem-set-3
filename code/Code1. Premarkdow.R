43ea4b9d4ed210bd8fc7301d18e2140301eb7364
sagujara <s.gutierrezj@uniandes.edu.co>
2023-05-17 23:10:25 -0500

    segundo modelo

--- code/Code1. Premarkdow.R
+++ code/Code1. Premarkdow.R
@@ -1,13 +1,15 @@
-# Samuel Gutiérrez Jaramillo
+# Nicolas Lozano - 
+# Samuel Gutiérrez Jaramillo - 202111674
 # TITULO:
 # R 4.2.2
 
-# If not installed run line 6.
+# If not installed run next line:
 # install.packages("pacman")
 
 require(pacman)
 p_load(tidyverse, rio, janitor, ggplot2, skimr, rvest, dplyr, +
-         tidyr, tibble, data.table)
+         tidyr, tibble, data.table, stargazer)
+rm(list=ls())
 
 #1.Regresiones.
 #Cargar base de datos
@@ -18,3 +20,6 @@ datareg <- import("input/data_regresiones.rds")
 #la vivienda a partir de cuartos, baños y área.
 m1 <- lm(price~rooms+bathrooms+surface_total, data=datareg)
 
+#Segundo modelo
+datareg <- mutate(datareg, tipo = ifelse(property_type == "Casa", 0, 1))
+m2 <- lm(price~tipo+bathrooms+rooms+surface_total, data=datareg)
