a91c8c80a6fbb609db93545c99a26e6cb94bc10c
Gustavo Lozano <gustavo.lozano@tecfinanzas.com>
2023-05-27 00:43:20 -0500

    Punto 1 final y avance 2

--- code/Code1. Premarkdow.R
+++ code/Code1. Premarkdow.R
@@ -7,10 +7,27 @@
 # install.packages("pacman")
 rm(list=ls())
 require(pacman)
-p_load(tidyverse, rio, janitor, ggplot2, skimr, rvest, dplyr, +
-         tidyr, tibble, data.table, stargazer, outreg, coefplot, xlsx, +
-         sf, leaflet,tmaptools, osmdata, ggsn, ggmap)
-rm(list=ls())
+p_load(tidyverse, 
+       rio, 
+       janitor, 
+       ggplot2, 
+       skimr, 
+       rvest, 
+       dplyr,
+       tidyr, 
+       tibble, 
+       data.table, 
+       stargazer, 
+       outreg, 
+       coefplot, 
+       xlsx,
+       sf, 
+       leaflet,
+       tmaptools, 
+       osmdata, 
+       ggsn, 
+       ggmap, 
+       modelsummary)
 
 #1.Regresiones.
 #Cargar base de datos
@@ -19,24 +36,40 @@ datareg <- import("input/data_regresiones.rds")
 #1.1. Estimaciones:
 #Primer modelo. Regresión lineal simple, explicar precio de 
 #la vivienda a partir de cuartos, baños y área.
-m1 <- lm(price~rooms+bathrooms+surface_total, data=datareg)
+m1 <- lm(price ~ rooms + bathrooms + surface_total, data = datareg)
 
 #Segundo modelo
-datareg <- mutate(datareg, tipo = ifelse(property_type == "Casa", 0, 1))
-m2 <- lm(price~rooms+bathrooms+tipo+surface_total, data=datareg)
+m2 <- lm(price ~ rooms + bathrooms + as.factor(property_type) + surface_total, data=datareg)
 
 #Tercer modelo
-m3 <- lm(price~tipo+bathrooms+rooms+surface_total+dist_cole+dist_park, data=datareg)
+m3 <- lm(price ~ as.factor(property_type) + bathrooms + rooms + surface_total + dist_cole + dist_park, data=datareg)
 
 #1.2 Tabla resultados:
-fitlist <- list(m1, m2, m3)
+fitlist <- list("Modelo 1" = m1, "Modelo 2" = m2, "Modelo 3" = m3)
+msummary(fitlist)
+
+#modelsummary(fitlist, stars = TRUE, output = "output\\modelos.xlsx")
+
 regs <- outreg(fitlist)
+export(regs, "output/resultados_regresiones.xlsx")
 
 #coefplots:
-plot <- multiplot(m1,m2,m3, title = "Estimación modelos",xlab = "Valor estimación del parámetro")
-ggsave("output/plot_regresiones.png")
 
-export(regs, "output/resultados_regresiones.xlsx")
+etiquetas <- c('dist_park' = 'Distancia Parques',
+               'rooms' = 'Habitaciones',
+               'bathrooms' = 'Baños',
+               'surface_total' = 'Area',
+               'dist_cole' = 'Distancia Colegios',
+               'as.factor(property_type)Casa' = 'Tipo Inmueble',
+               '(Intercept)' = 'Intercepto')
+
+
+modelplot(fitlist, coef_map = etiquetas, facet = F) + 
+  labs(title = "Coeficientes Estimados", subtitle = "Intervalos 95%", caption="", alt = "") +
+  scale_x_continuous(labels = scales::comma) + 
+  theme_grey() 
+ggsave("output/plot_regresiones.png", width = 7, height = 5, dpi = 300, units = "in")
+
 
 #2. Datos espaciales:
 #2.1 Descargar datos
@@ -51,13 +84,29 @@ osm_sf <- osm %>% osmdata_sf()
 parques <- osm_sf$osm_polygons %>% select(osm_id,leisure)
 
 #2.2 Visualizar
-leaflet() %>% addTiles() %>% addCircleMarkers(data=restaurantes, col="blue") %>% addPolygons(data=parques, col="green")
+leaflet() %>% 
+  addTiles() %>% 
+  addCircleMarkers(data=restaurantes, col="blue") %>% 
+  addPolygons(data=parques, col="green")
 
 #2.3 Geocodificar sitio: Bicicleteria KIKO
 bici <- geocode_OSM("Calle 6 %7% 7-6, Ubate Colombia", as.sf=T)
 
 #2.4 Mapa
-Ubate <- opq("Ubate Colombia") %>%
-  add_osm_feature(key="amenity" , value="restaurant") %>% add_osm_feature(key="leisure", value="park") %>% osmdata_sf()
+ubate <- opq(bbox = getbb("Ubate Colombia")) %>%
+  add_osm_feature(key="boundary", value="administrative") %>%
+  osmdata_sf()
+
+ubate <- ubate$osm_multipolygons %>% subset(admin_level==6)
+
+osm_layer <- get_stamenmap(bbox = as.vector(st_bbox(ubate)), 
+                          maptype="toner", source="osm", zoom=13) 
+
+ggmap(osm_layer) + geom_sf(data = ubate,alpha=0.3, inherit.aes=F) +
+  geom_sf(data = parques, colour = 'red',size = 15, inherit.aes = FALSE)+
+  geom_sf(data = restaurantes, colour = 'blue',size = 1, inherit.aes = FALSE)+
+  scalebar(data = ubate , dist = 5 , transform = T , dist_unit = "km") +
+  north(data = ubate , location = "topleft") + theme_linedraw() + labs(x="" , y="")
+
 
   
